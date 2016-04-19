cb17372ef1eef615b3da6e24b92cd76c22d37a31
Thomas W <netzberufler@googlemail.com>
2016-04-19 11:00:31 +0200

    forked from zeeDynamic

---
+++ js/slider.js
@@ -0,0 +1,22 @@
+/**
+ * jQuery Slider JS
+ *
+ * Adds the Flexslider Plugin for the Featured Post Slideshow
+ *
+ * @package zeeDynamic
+ */
+
+jQuery(document).ready(function($) {
+
+	/* Add flexslider to #post-slider div */ 
+	$("#post-slider").flexslider({
+		animation: zeedynamic_slider_params.animation,
+		slideshowSpeed: zeedynamic_slider_params.speed,
+		namespace: "zeeflex-",
+		selector: ".zeeslides > li",
+		smoothHeight: true,
+		pauseOnHover: true,
+		controlsContainer: ".post-slider-controls"
+	});
+	
+});
