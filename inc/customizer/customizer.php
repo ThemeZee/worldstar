7a6d85b3bf1b414984b4c54ffbc44bde7b391125
Thomas W <kontakt@themecoder.de>
2018-04-13 19:22:22 +0200

    moved assets into new folder

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -110,7 +110,7 @@ function worldstar_customize_partial_blogdescription() {
  * Embed JS file to make Theme Customizer preview reload changes asynchronously.
  */
 function worldstar_customize_preview_js() {
-	wp_enqueue_script( 'worldstar-customizer-preview', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20170627', true );
+	wp_enqueue_script( 'worldstar-customizer-preview', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), '20170627', true );
 }
 add_action( 'customize_preview_init', 'worldstar_customize_preview_js' );
 
@@ -119,7 +119,7 @@ add_action( 'customize_preview_init', 'worldstar_customize_preview_js' );
  * Embed CSS styles for the theme options in the Customizer
  */
 function worldstar_customize_preview_css() {
-	wp_enqueue_style( 'worldstar-customizer-css', get_template_directory_uri() . '/css/customizer.css', array(), '20161214' );
+	wp_enqueue_style( 'worldstar-customizer-css', get_template_directory_uri() . '/assets/css/customizer.css', array(), '20161214' );
 }
 add_action( 'customize_controls_print_styles', 'worldstar_customize_preview_css' );
 
